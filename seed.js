d87f39e687ee85d937567d7e44f0b39edd36902e
zNadir <115375151+znadir@users.noreply.github.com>
2024-04-03 23:31:49 -0400

    Add seed script to package.json

---
+++ seed.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import Client from "./models/client.js";
+import CodePromo from "./models/codePromo.js";
+import Collection from "./models/collection.js";
+import Produit from "./models/produit.js";
+
+(async () => {
+	const db = mongoose.connection;
+
+	db.on("error", (err) => {
+		console.error("Error connecting to MongoDB:", err);
+	});
+
+	db.once("open", () => {
+		console.log("Connected to MongoDB");
+	});
+
+	await mongoose.connect("mongodb://localhost:27017/db");
+
+	// clear the database
+	console.log("Clearing the database...");
+	await db.dropDatabase();
+
+	console.log("Seeding...");
+
+	await Client.create([
+		{
+			prenom: "John",
+			nom: "Doe",
+			email: "john@example.com",
+			mdpHash: "password123",
+		},
+	]);
+
+	await CodePromo.create([
+		{
+			rabaisPourcent: 10,
+			dateExpiration: new Date("2021-12-31"),
+		},
+	]);
+
+	const collectionChandail = await Collection.create({ nom: "Chandail" });
+	const collectionChaussure = await Collection.create({ nom: "Chaussures" });
+
+	await Produit.create([
+		{
+			nom: "Chemise décontractée",
+			description: "Chemise en coton confortable à porter au quotidien.",
+			marque: "XYZ Brand",
+			taille: "M",
+			genre: "H",
+			prix: 29.99,
+			collectionId: collectionChandail._id,
+		},
+		{
+			nom: "Chaussures de course",
+			description: "Chaussures légères avec une excellente traction pour la course.",
+			marque: "ABC Sportswear",
+			taille: "L",
+			genre: "F",
+			prix: 59.99,
+			collectionId: collectionChaussure._id,
+		},
+	]);
+
+	// close
+	await mongoose.disconnect();
+})();
