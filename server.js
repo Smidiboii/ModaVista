9a3e5f520e3a7f571ac80bd55f6e4d4db19c8909
MathieuMoukarzel2268496 <118761310+MathieuMoukarzel2268496@users.noreply.github.com>
2024-04-10 15:06:34 -0400

    signin fait
    le signin fonctionne avec mongo maintenant

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
 import mysql from "mysql";
+import { MongoClient } from "mongodb";
 
 /*
     Configuration de EJS
@@ -30,7 +31,22 @@ con.connect(function (err) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/*
+    Connection à MongoDB
+*/
+const uri = "mongodb+srv://scott:oracle@bdmodavista.welwlkh.mongodb.net/";
+const client = new MongoClient(uri);
 
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error('Erreur avec la consultation de la BD.', err);
+    }
+}
+
+connectToMongoDB();
 /*
     Description des routes
 */
@@ -48,60 +64,116 @@ app.get("/", function (req, res) {
 
 })
 
+app.get("/signup", function (req, res) {
+    res.render("pages/signup");
+});
+
+app.get("/collection/:id", async (req, res) => {
+    let idFromParams = req.params.id;
+    console.log(idFromParams);
+
+    try {
+        const db = client.db('Modavista');
+        const ListeProduits = db.collection('Produit');
+        const produits = await ListeProduits.find({ 'CollectionID': { $eq: Number(idFromParams) } }).project({ 'Nom': 1, 'Prix': 1, 'ProduitID': 1 }).toArray();
+        const collections = await ObtenirCollections(idFromParams, res);
+        console.log(produits);
+
+        MontrerCollection(produits, collections[0], collections[1], res, idFromParams);
+    } catch (err) {
+        console.error('Erreur avec la consultation de la BD.', err);
+
+    }
+});
+
+app.post("/signup", async function (req, res) {
+    const { prenom, nom, email, mdp, tel, adresse } = req.body;
+    const db = client.db('Modavista');
+    const listeClient = db.collection("Client");
+    const ClientDoc = {
+        Prenom: prenom,
+        Nom: nom,
+        Email: email,
+        Mdp: mdp,
+        Telephone: tel,
+        Adresse: adresse
+    };
+    try{
+        listeClient.insertOne(ClientDoc);        
+        res.json({ message: "success" });
+    }finally{
+        console.log(await findClient(listeClient,prenom));
+    }
+});                     
+export async function findClient(collection, nom){
+    return collection.find({nom}).toArray();
+}
+
+app.get("/login", function (req, res) {
+    res.render("pages/login");
+})
+
+export async function connexionClient(collection, email, password){
+    collection.find({email, password}).toArray();
+}
+var Email;
+var Password;
+app.post("/login", async function (req, res) {
+    const { email, password } = req.body;
+
+    const db = client.db('Modavista');
+    const listeClient = db.collection("Client");
+    try{
+        await connexionClient(listeClient, email, password);
+    }finally{
+        Email = email;
+        Password = password;
+    }
+});
+
 app.get("/compte", function (req, res) {
     res.render("pages/compte");
 })
 
-app.get("/collection", function (req, res) {
+app.post("/compte",async function (req,res){
+    const db = client.db('Modavista');
+    const listeClient = db.collection("Client");
+    const { prenomC, nomC } = req.body;
 
-    con.query('SELECT NomCollection, CollectionID FROM Collection', (err, results) => {
-        if (err) {
-            console.error('Erreur avec la consultation de la BD.', err);
-            return res.status(500).send('Collections non trouvées.');
-        }
-        con.query('SELECT Nom, Prix, ProduitID FROM Produit', (err, produits) => {
-            MontrerCollection(produits, "Nos Produits", results, err, res);
-        });
-    });
+    try{
+        await connexionClient(listeClient, email, password);
+    }finally{
+        prenomC = 
+    }
 });
 
-app.get("/collection/:id", function (req, res) {
-    let idFromParams = req.params.id;
-    console.log(idFromParams);
-
-    con.query('SELECT NomCollection, CollectionID FROM Collection', (errCollection, collections) => {
-        if (errCollection) {
-            console.error('Erreur avec la consultation de la BD.', errCollection);
-            return res.status(500).send('Collections non trouvées.');
-        }
-
-        var NomCollection = "";
-        for (let index = 0; index < collections.length; index++) {
-            if (collections[index].CollectionID == idFromParams) {
-                NomCollection = collections[index].NomCollection;
-
-                break;
+function ObtenirCollections(idFromParams, res) {
+    return new Promise((resolve, reject) => {
+        con.query('SELECT NomCollection, CollectionID FROM Collection', (errCollection, collections) => {
+            if (errCollection) {
+                console.error('Erreur avec la consultation de la BD.', errCollection);
+                res.status(500).send('Collections non trouvées.');
+                reject(errCollection);
+            } else {
+                var NomCollection = "";
+                for (let index = 0; index < collections.length; index++) {
+                    if (collections[index].CollectionID == idFromParams) {
+                        NomCollection = collections[index].NomCollection;
+                        break;
+                    }
+                }
+                var data = [NomCollection, collections];
+                resolve(data);
             }
-        }
-
-        con.query('SELECT Nom, Prix, ProduitID FROM Produit where CollectionID = ?', idFromParams, (errProduct, produits) => {
-
-            MontrerCollection(produits, NomCollection, collections, errProduct, res);
         });
     });
-})
+}
 
-function MontrerCollection(ListeProduits, NomCollection, results, err, res) {
-    if (err) {
-        console.error('Erreur avec la consultation de la BD.', err);
-        return res.status(500).send('Collection non trouvée.');
-    }
+function MontrerCollection(ListeProduits, NomCollection, results, res, idFromParams) {
+    console.log(NomCollection);
     var hrefFondDEcran = "assets/img/" + NomCollection + ".jpg";
     if (NomCollection == "Nos Produits") {
         hrefFondDEcran = "../" + hrefFondDEcran;
-    }
-    for (let index2 = 0; index2 < ListeProduits.length; index2++) {
-        console.log(ListeProduits[index2]);
     }
     res.render("pages/collection.ejs", {
         fondDEcran: "assets/img/" + NomCollection + ".jpg",
@@ -124,49 +196,8 @@ app.get("/produit/:id", function (req, res) {
     });
 });
 
-app.get("/login", function (req, res) {
-    res.render("pages/login");
-})
-
-app.get("/signup", function (req, res) {
-    res.render("pages/signup");
-});
 
 
-app.post("/login", function (req, res) {
-    const { email, password } = req.body;
-
-    con.query("SELECT * FROM Client WHERE Email = ? AND Mdp = ?", [email, password], function (err, result) {
-        if (err) throw err;
-        if (result.length > 0) {
-            res.json({ message: "success" });
-        } else {
-            res.json({ message: "failed" });
-        }
-    });
-});
-
-app.post("/signup", function(req, res) {
-    const { prenom, nom, email, mdp, tel, adresse } = req.body;
-    con.query("SELECT * FROM Client WHERE Email = ?", [email], function(err, result) {
-        if (err) {
-            res.status(500).json({ message: "Erreur interne du serveur" });
-            throw err;
-        }
-        if (result.length > 0) {
-            res.json({ message: "L'email existe déjà. Veuillez choisir un autre email." });
-        } else {
-            con.query("INSERT INTO Client (Prenom, Nom, Email, Mdp, TelNum, Adresse) VALUES (?, ?, ?, ?, ?, ?)", [prenom, nom, email, mdp, tel, adresse], function(err, result) {
-                if (err) {
-                    res.status(500).json({ message: "Erreur interne du serveur" });
-                    throw err;
-                }
-
-                res.json({ message: "success" });
-            });
-        }
-    });
-});
 
 app.listen(3000, function () {
     console.log("serveur fonctionne sur http://localhost:3000 ... ! ");
